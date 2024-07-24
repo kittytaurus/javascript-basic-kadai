88fded5cc4426b247bfbd83be60a422eb0e20a4b
kittytaurus <sweetaurusberry@gmail.com>
2024-07-24 15:09:19 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,5 @@
+const moonLanding = new Date('September 22, 23 00:00:00');
+
+console.log(moonLanding.getFullYear()+ "年");
+console.log(moonLanding.getMonth()+"月");
+console.log(moonLanding.getDate()+"日");
